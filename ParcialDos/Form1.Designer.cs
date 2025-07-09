f1dfcbbb90865676be27be55237ba23f271e52e1
Ivan Lopez <ialmendra@infobae.com>
2025-07-08 22:22:50 -0300

    Agregar archivos de proyecto.

---
+++ ParcialDos/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ParcialDos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerarComprobantes = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lstResultadoComprobantes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerarComprobantes
+            // 
+            this.btnGenerarComprobantes.Location = new System.Drawing.Point(22, 27);
+            this.btnGenerarComprobantes.Name = "btnGenerarComprobantes";
+            this.btnGenerarComprobantes.Size = new System.Drawing.Size(139, 40);
+            this.btnGenerarComprobantes.TabIndex = 0;
+            this.btnGenerarComprobantes.Text = "Generar comprobantes";
+            this.btnGenerarComprobantes.UseVisualStyleBackColor = true;
+            this.btnGenerarComprobantes.Click += new System.EventHandler(this.btnGenerarComprobantes_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(22, 73);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(139, 40);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click_1);
+            // 
+            // lstResultadoComprobantes
+            // 
+            this.lstResultadoComprobantes.FormattingEnabled = true;
+            this.lstResultadoComprobantes.Location = new System.Drawing.Point(199, 27);
+            this.lstResultadoComprobantes.Name = "lstResultadoComprobantes";
+            this.lstResultadoComprobantes.Size = new System.Drawing.Size(507, 394);
+            this.lstResultadoComprobantes.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 434);
+            this.Controls.Add(this.lstResultadoComprobantes);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnGenerarComprobantes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerarComprobantes;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ListBox lstResultadoComprobantes;
+    }
+}
+
---
+++ ParcialDos/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ParcialDos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Estructura para almacenar detalles
+        class Detalle
+        {
+            public string NroComprobante { get; set; }
+            public string Descripcion { get; set; }
+            public int Importe { get; set; }
+        }
+
+        private void btnGenerarComprobantes_Click(object sender, EventArgs e)
+        {
+            lstResultadoComprobantes.Items.Clear();
+
+            // Leer todos los detalles de una sola vez
+            List<Detalle> detalles = new List<Detalle>();
+            using (var sr = new StreamReader("detalle.txt"))
+            {
+                string linea;
+                bool esPrimera = true;
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    if (esPrimera) { esPrimera = false; continue; }
+
+                    var partes = linea.Split(';');
+                    detalles.Add(new Detalle
+                    {
+                        NroComprobante = partes[0],
+                        Descripcion = partes[1],
+                        Importe = int.Parse(partes[2])
+                    });
+                }
+            }
+
+            // Leer la cabecera
+            using (var sr = new StreamReader("cabecera.txt"))
+            {
+                string linea;
+                bool esPrimera = true;
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    if (esPrimera) { esPrimera = false; continue; }
+
+                    var partes = linea.Split(';');
+                    string nroComprobante = partes[0];
+                    string razonSocial = partes[1];
+                    string fecha = partes[2];
+
+                    lstResultadoComprobantes.Items.Add($"Comprobante: {nroComprobante}");
+                    lstResultadoComprobantes.Items.Add($"Razón Social: {razonSocial}");
+                    lstResultadoComprobantes.Items.Add($"Fecha: {fecha}");
+
+                    int total = 0;
+
+                    var detallesComprobante = detalles.Where(d => d.NroComprobante == nroComprobante).ToList();
+                    foreach (var d in detallesComprobante)
+                    {
+                        lstResultadoComprobantes.Items.Add($"    - {d.Descripcion} - ${d.Importe}");
+                        total += d.Importe;
+                    }
+
+                    lstResultadoComprobantes.Items.Add($"TOTAL: ${total}");
+                    lstResultadoComprobantes.Items.Add("--------------------------------------"); // línea en blanco entre comprobantes
+                }
+            }
+        }
+
+        private void btnSalir_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
